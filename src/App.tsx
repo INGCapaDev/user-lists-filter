c4868102de8e32718405a13aa796a75e90d31808
Alvaro Capaceta <ingcapadev@gmail.com>
2023-10-30 13:47:01 -0700

    feat: Loading state

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   const [sorting, setSorting] = useState<SortBy>(SortBy.NONE);
   const [filterCountry, setFilterCountry] = useState<string | null>(null);
 
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const originalUsers = useRef<User[]>([]);
   // useRef -> para guardar un valor
   // que queremos que se comparta entre renderizados
@@ -38,14 +41,20 @@ function App() {
   };
 
   useEffect(() => {
-    fetch('https://randomuser.me/api?results=100')
+    setLoading(true);
+    setError(false);
+    fetch('https://randomuser.me/api?results=10')
       .then(async (res) => await res.json())
       .then((res) => {
         setUsers(res.results);
         originalUsers.current = res.results;
       })
       .catch((err) => {
+        setError(true);
         console.error(err);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
@@ -118,12 +127,21 @@ function App() {
         />
       </header>
       <main>
-        <UsersList
-          changeSorting={handleChangeSort}
-          deleteUser={handleDelete}
-          showColors={showColors}
-          users={sortedUsers}
-        />
+        {loading && (
+          <div>
+            <strong>Cargando ...</strong>
+          </div>
+        )}
+        {!loading && error && <div>Hubo un error</div>}
+        {!loading && !error && users.length === 0 && <div>No hay usuarios</div>}
+        {!loading && !error && users.length > 0 && (
+          <UsersList
+            changeSorting={handleChangeSort}
+            deleteUser={handleDelete}
+            showColors={showColors}
+            users={sortedUsers}
+          />
+        )}
       </main>
     </div>
   );
